5293072e59a2a99358cde1b93c36a6d18089e551
Dimitri <gitmitri@gmail.com>
2024-02-26 10:28:35 +0700

    fix: content height

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         setCurrentScreen('random-episode')
     }
     return (
-        <div className="flex h-screen w-full items-center justify-center">
+        <div className="flex h-dvh w-full items-center justify-center">
             {currentScreen === 'home' && (
                 <Home onGenerateRandomEpisode={handleGenerateRandomEpisode} />
             )}
